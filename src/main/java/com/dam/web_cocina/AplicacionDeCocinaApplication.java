c405f180fe8dcb494e52d1389ca7011ad9296a04
rover <110656154+Irhis13@users.noreply.github.com>
2025-04-19 21:34:19 +0200

    Commit inicial

---
+++ src/main/java/com/dam/web_cocina/AplicacionDeCocinaApplication.java
@@ -0,0 +1,13 @@
+package com.dam.web_cocina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionDeCocinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionDeCocinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dam/web_cocina/AplicacionDeCocinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam.web_cocina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionDeCocinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
